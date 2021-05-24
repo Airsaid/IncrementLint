1b5e2f820b75ee0e96aa101aeec5ea7423d86452
Airsaid <airsaid1024@gmail.com>
2021-05-24 15:37:00 +0800

    Add maven-publish plugin

--- incrementlint/src/main/java/com/airsaid/incrementlint/IncrementLintPlugin.kt
+++ incrementlint/src/main/java/com/airsaid/incrementlint/IncrementLintPlugin.kt
@@ -6,8 +6,8 @@ import org.gradle.api.Project
 /**
  * @author airsaid
  */
-class IncrementLintPlugin : Plugin<Project> {
+open class IncrementLintPlugin : Plugin<Project> {
   override fun apply(project: Project) {
-    
+    println("apply: $project")
   }
 }
