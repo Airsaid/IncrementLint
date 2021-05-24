fd9da527095b13698bbae26f759f41e0bd74ed99
Airsaid <airsaid1024@gmail.com>
2021-05-24 15:13:34 +0800

    Add java gradle plugin

---
+++ incrementlint/src/main/java/com/airsaid/incrementlint/IncrementLintPlugin.kt
@@ -0,0 +1,13 @@
+package com.airsaid.incrementlint
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+/**
+ * @author airsaid
+ */
+class IncrementLintPlugin : Plugin<Project> {
+  override fun apply(project: Project) {
+    
+  }
+}
